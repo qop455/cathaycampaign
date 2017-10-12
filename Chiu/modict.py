bbcc58db2f7eb7914e39bbbb8d1212a75e2b23ba
Jason Chiu <qop455@gmail.com>
2017-10-12 17:10:16 +0800

    cecsus to dummy csv

--- Chiu/modict.py
+++ Chiu/modict.py
@@ -1,26 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+@author: jason
+"""
+
 from numpy import nan
 
 def get_dummy_dict():
     result = dict()
     result['Region'] = {'E12000001':'NorthEast', 'E12000002':'NorthWest', 'E12000003':'YorkshireHumber', 'E12000004':'EastMidlands', 'E12000005':'WestMidlands', 'E12000006':'EastEngland', 'E12000007':'London', 'E12000008':'SouthEast', 'E12000009':'SouthWest', 'W92000004':'Wales'}
     result['Residence Type'] = {'C':'CommunalEstablishment', 'H':'NotCommunalEstablishment'}
-    result['Family Composition'] = {1:'NotInFamily', 2:'Married', 3:'Cohabiting', 4:'LoneParentMale', 5:'LoneParentFemale', 6:'Other', -9:'NotRequired'}
-    result['Population Base'] = {1:'Usual', 2:'StudentLivingAway', 3:'ShortTime'}
-    result['Sex'] = {1:'Male', 2:'Female'}
-    result['Marital Status'] = {1:'Single', 2:'Married', 3:'Separated', 4:'Divorced', 5:'Widowed'}
+    result['Family Composition'] = {1:'Family_NotInFamily', 2:'Family_Married', 3:'Family_Cohabiting', 4:'Family_LoneParentMale', 5:'Family_LoneParentFemale', 6:'Family_Other', -9:'NotRequired'}
+    result['Population Base'] = {1:'PopulationBase_Usual', 2:'PopulationBase_StudentLivingAway', 3:'PopulationBase_ShortTime'}
+    result['Sex'] = {1:'Sex_Male', 2:'Sex_Female'}
+    result['Marital Status'] = {1:'MaritalStatus_Single', 2:'MaritalStatus_Married', 3:'MaritalStatus_Separated', 4:'MaritalStatus_Divorced', 5:'MaritalStatus_Widowed'}
     result['Student'] = {1:'Student', 2:'NotStudent'}
-    result['Country of Birth'] = {1:'UK', 2:'NonUK', -9:'NotRequired'}
-    result['Health'] = {1:'VeryGood', 2:'Good', 3:'Fair', 4:'Bad', 5:'VeryBad', -9:'NotRequired'}
-    result['Ethnic Group'] = {1:'White', 2:'Mixed', 3:'Asian', 4:'Black', 5:'ChineseOrOther', -9:'NotRequired'}
-    result['Religion'] = {1:'NoReligion', 2:'Christian', 3:'Buddhist', 4:'Hindu', 5:'Jewish', 6:'Muslim', 7:'Sikh', 8:'Other', 9:'NotStated', -9:'NotRequired'}
-    result['Economic Activity'] = {1:'Employee', 2:'SelfEmployed', 3:'Unemployed', 4:'FullTimeStudent', 5:'Retired', 6:'Student', 7:'LookingAfterHome', 8:'Disabled', 9:'Other', -9:'NotRequired'}
-    result['Approximated Social Grade'] = {1:'AB', 2:'C1', 3:'C2', 4:'DE', -9:'NotRequired'}
+    result['Country of Birth'] = {1:'Birth_UK', 2:'Birth_NonUK', -9:'NotRequired'}
+    result['Ethnic Group'] = {1:'EthnicGroup_White', 2:'EthnicGroup_Mixed', 3:'EthnicGroup_Asian', 4:'EthnicGroup_Black', 5:'EthnicGroup_ChineseOrOther', -9:'NotRequired'}
+    result['Religion'] = {1:'Religion_NoReligion', 2:'Religion_Christian', 3:'Religion_Buddhist', 4:'Religion_Hindu', 5:'Religion_Jewish', 6:'Religion_Muslim', 7:'Religion_Sikh', 8:'Religion_Other', 9:'Religion_NotStated', -9:'NotRequired'}
+    result['Economic Activity'] = {1:'EconomicActivity_Employee', 2:'EconomicActivity_SelfEmployed', 3:'EconomicActivity_Unemployed', 4:'EconomicActivity_FullTimeStudent', 5:'EconomicActivity_Retired', 6:'EconomicActivity_Student', 7:'EconomicActivity_LookingAfterHome', 8:'EconomicActivity_Disabled', 9:'EconomicActivity_Other', -9:'NotRequired'}
+    result['Approximated Social Grade'] = {1:'SocialGrade_AB', 2:'SocialGrade_C1', 3:'SocialGrade_C2', 4:'SocialGrade_DE', -9:'NotRequired'}
     return result
 
 def get_value_dict():
     result = dict()
     result['Age'] = {1:7.5, 2:20, 3:29.5, 4:39.5, 5:49.5, 6:59.5, 7:69.5, 8:75}
-    result['Hours worked per week'] = {1:7.5, 2:23, 3:39.5, 4:49, 5:0, -9:nan}
+    result['Hours worked per week'] = {1:7.5, 2:23, 3:39.5, 4:49, -9:0}
     return result
 
 def main():
--- Chiu/preprocess.py
+++ Chiu/preprocess.py
@@ -4,11 +4,10 @@
 """
 
 import pandas as pd
-import numpy as np
 import modict as mo
 
-
 data = pd.read_csv("../raw_data/Census_Microdata.csv")
+data.drop(['Person ID', 'Health'], inplace=True, axis=1)
 
 feature_dummy_dict = mo.get_dummy_dict()
 feature_value_dict = mo.get_value_dict()
@@ -16,9 +15,19 @@ feature_value_dict = mo.get_value_dict()
 for key, value in feature_dummy_dict.items():
     new_dummy = pd.get_dummies(data[key])
     new_dummy = new_dummy.rename(columns=value)
+    data.drop([key], inplace=True, axis=1)
     data = pd.concat([data, new_dummy], axis=1)
 
-for key, value in feature_value_dict.items():
-    for k, v in value.items():
-        data[key][data[key] == k].set
+data.drop(['NotRequired'], inplace=True, axis=1)
 
+for key, value in feature_value_dict.items():
+    data[key] = data[key].map(value)
+
+Occupation = pd.get_dummies(data['Occupation']).rename(columns = lambda col: 'Occupation_%s'%col)
+Industry = pd.get_dummies(data['Industry']).rename(columns = lambda col: 'Industry_%s'%col)
+
+data = pd.concat([data, Occupation, Industry], axis=1)
+
+data.drop(['Occupation', 'Industry', 'Occupation_-9', 'Industry_-9'], inplace=True, axis=1)
+
+data.to_csv('../data/dummy_data.csv', index=False)
